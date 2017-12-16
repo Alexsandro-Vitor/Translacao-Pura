3faf17e9a534a9878aa0e27f5627667e59582d00
Alexsandro Vítor <avsc@cin.ufpe.br>
2017-12-16 17:21:39 -0300

    Criação do projeto Unity

---
+++ Assets/Scripts/ControleCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float velX = -Input.GetAxis("Vertical");
+		float velY = Input.GetAxis("Horizontal");
+		Vector3 rotacao = transform.eulerAngles;
+		transform.eulerAngles = rotacao + new Vector3 (velX, velY, 0f);
+	}
+}
---
+++ Assets/Scripts/MovimentoBolas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoBolas : MonoBehaviour {
+	private Rigidbody rb;
+	public float zOrig;
+	public Vector3 velocidade = new Vector3(0f, 0f, 1f);
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = new Vector3(0f, 0f, 1f);
+		zOrig = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector3(0f, 0f, zOrig + (transform.position.z - zOrig) % 1);
+	}
+}
